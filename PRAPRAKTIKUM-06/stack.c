c77f60446110eebcc4f4d91c625c89cb40f15cc2
stargeaze <yasminfsalma@gmail.com>
2023-10-05 07:51:58 +0700

    feat stack.c

---
+++ PRAPRAKTIKUM-06/stack.c
@@ -0,0 +1,59 @@
+// Nama : Yasmin Farisah Salma
+// NIM  : 13522140
+// Prapraktikum 6 Algoritma dan Struktur Data
+
+
+#include <stdio.h>
+#include "boolean.h"
+#include "stack.h"
+
+
+
+/* ************ Prototype ************ */
+/* *** Konstruktor/Kreator *** */
+void CreateEmpty(Stack *S)
+/* I.S. sembarang; */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* jadi indeksnya antara 0.. MaxEl */
+/* Ciri stack kosong : TOP bernilai Nil */
+{ 
+  Top(*S) = Nil;
+}
+
+
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+boolean IsEmpty(Stack S)
+/* Mengirim true jika Stack kosong: lihat definisi di atas */
+{
+  return (Top(S) == Nil);
+}
+
+
+boolean IsFull(Stack S)
+/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+{
+    return (Top(S) == (MaxEl - 1));
+}
+
+
+/* ************ Menambahkan sebuah elemen ke Stack ************ */
+void Push(Stack * S, infotype X)
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
+/* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
+{
+    Top(*S)++;
+    InfoTop(*S) = X;
+}
+
+
+
+/* ************ Menghapus sebuah elemen Stack ************ */
+void Pop(Stack * S, infotype* X)
+/* Menghapus X dari Stack S. */
+/* I.S. S  tidak mungkin kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
+{
+    *X = InfoTop(*S);
+    Top(*S)--;
+}
