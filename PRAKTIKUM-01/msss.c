ac7db9763ce1ec4298da9e5d826d1e0966d6899b
stargeaze <yasminfsalma@gmail.com>
2023-12-01 10:50:26 +0700

    fix msss.c

--- PRAKTIKUM-01/msss.c
+++ PRAKTIKUM-01/msss.c
@@ -3,29 +3,32 @@
 // Praktikum 1 Algoritma dan Struktur Data
 
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 #include "point.h"
 
+
 int main() {
-    POINT P[3]; 
-    int i;
+    POINT a, b, c;
+    BacaPOINT(&a);
+    BacaPOINT(&b);
+    BacaPOINT(&c);
 
-    for (i = 0; i < 3; i++) {
-        BacaPOINT(&P[i]);
-    }
+    float y1 = Ordinat(c) - Ordinat(b),
+    y2 = Ordinat(b) - (Ordinat(a)),
+    b1 = Absis(c) - Absis(b),
+    b2 = Absis(b) - Absis(a),
+    c1 = pow(Absis(c), 2) - pow(Absis(b), 2),
+    c2 = pow(Absis(b), 2) - pow(Absis(a), 2);
 
-    float a = 0;
+    float tempc = c2 * b1 / b2,
+    tempy = y2 * b1 / b2;
 
-    for (i = 0; i < 3; i++) {
-        float Li = 1;
-        for (int j = 0; j < 3; j++) {
-            if (i != j) {
-                Li *= (0 - Absis(P[j])) / (Absis(P[i]) - Absis(P[j]));
-            }
-        }
-        a += Li * Ordinat(P[i]);
-    }
+    float cc = (y1 - tempy) / (c1 - tempc),
+    bb = (tempy - (tempc * cc)) / b1,
+    aa = Ordinat(a) - (Absis(a) * bb) - (pow(Absis(a), 2) * cc);
 
-    printf("%.0f\n", a);
-
+    printf("%.0f\n", aa);
+    
     return 0;
 }
